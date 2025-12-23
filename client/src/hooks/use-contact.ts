fc49173e956809a05a17e8e03540f764c4882688
techsouls <141745312+techsouls@users.noreply.github.com>
2025-12-23 16:26:55 +0800

    commit error

--- client/src/hooks/use-contact.ts
+++ client/src/hooks/use-contact.ts
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
-import { api, type InsertContactMessage } from "@shared/routes";
+// import { api, type InsertContactMessage } from "@shared/routes";
+import { api } from "@shared/routes";
 import { useToast } from "@/hooks/use-toast";
 
 export function useCreateContactMessage() {
